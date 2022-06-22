3071adb1219b294e648533895f3163ef294a65bb
ElephantGame1 <102257618+ElephantGame1@users.noreply.github.com>
2022-06-22 20:13:25 +0300

    Add files via upload

---
+++ pyDEL/pyDEL/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace pyDEL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd.exe");
+        }
+
+        private void openTheThemeEditorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Setting set = new Setting();
+            set.DoText = fastColoredTextBox1.Text;
+            set.ScaleMD = Convert.ToString(documentMap1.Scale);
+            set.ScaleMSW = Convert.ToString(documentMap1.Size.Width);
+            set.SizeX = Size.Width;
+            set.SizeY = Size.Height;
+            set.Show();
+        }
+    }
+}
---
+++ pyDEL/pyDEL/Setting.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pyDEL
+{
+    public partial class Setting : Form
+    {
+        public int SizeX;
+        public int SizeY;
+
+        public float ScaleMapDoc;
+        public string ScaleMD;
+        public string ScaleMSW;
+
+        public string DoText;
+
+        public Setting()
+        {
+            InitializeComponent();
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            button3.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            button3.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Text = ScaleMD;
+            textBox2.Text = ScaleMSW;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            button3.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            button3.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            ScaleMapDoc = Convert.ToSingle(textBox1.Text);
+            Form1 f1 = new Form1();
+            f1.documentMap1.Scale = ScaleMapDoc;
+            f1.fastColoredTextBox1.Text = DoText;
+            f1.documentMap1.Size = new Size(Convert.ToInt32(textBox2.Text), f1.documentMap1.Size.Height);
+            f1.Size = new Size(SizeX, SizeY);
+            f1.Show();
+            Hide();
+        }
+    }
+}
